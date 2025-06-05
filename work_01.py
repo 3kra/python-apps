628d313f14a0094ab54c03b38f88dd0486564835
3kra <saku1230ra@icloud.com>
2025-06-05 11:42:03 +0900

    ゲームを続けるかやめるか選べるようにした

---
+++ work_01.py
@@ -0,0 +1,50 @@
+# input_line = input("何か入れて：")
+# print(input_line)
+
+import random
+
+while True:
+
+    answer = random.randint(1, 100)
+    # print(answer)
+
+    result = False
+    cnt = 0
+    for i in range(5):
+        cnt = cnt + 1
+        input_line = int(input("1〜100までの数字を入力してください："))
+    # 入力された文字があってたら→「当たり！」、外れてたら→「不正解」を表示
+        if input_line == answer:
+                print(f"正解です！")
+                result = True
+                break   
+        else:
+                print("不正解です!")
+                # ヒントを出す
+                if input_line < answer:
+                    print("もっと大きい数字です")
+                    
+                    # どのくらい大きいか教える
+                    if answer - input_line > 10:
+                        print("10以上大きいです")
+                    else:
+                        print("もっと大きい数字ですが、だいぶ近いです")
+                else:
+                    if input_line - answer > 10:
+                        print("10以上小さいです")
+                    else:
+                        print("もっと小さいですが、だいぶ近いです")
+
+# ５回全部外れたら最後に答えを表示する
+# if answer != input_line:
+#     print(f"正解は{answer}でした！")
+
+#もう一度チャレンジするかきく
+
+    if result:
+        print(f"ゲームに勝ちました。あなたは{cnt}回目で正解しました。")
+    else:
+        print(f"ゲームに負けました。正解は{answer}でした。")
+
+    ans = input("もう一度やりますか？（yes/no):")
+    if ans = 
